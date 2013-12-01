9fa20393bbfda1cd262bc2db66df8748647b94f4
Scott Forman <sjforman@gmail.com>
2013-11-30 21:56:28 -0500

    added favicons

---
+++ favicon_make.rb
@@ -0,0 +1,14 @@
+require "rubygems"
+require "favicon_maker"
+
+options = {
+  :versions => [:apple_144, :apple_120, :apple_114, :apple_72, :apple_57, :apple_pre, :apple, :fav_png, :fav_ico],
+  :custom_versions => {},
+  :root_dir => File.dirname(__FILE__),
+  :input_dir => "favicons",
+  :base_image => "favicon_base.png",
+  :output_dir => "favicons_output",
+  :copy => false
+}
+
+FaviconMaker::Generator.create_versions(options)
